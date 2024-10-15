6057026d880619ba6ac5d8ac5af4f8ad7ec842b9
Matt Buczkowski <matbuczkowski95@gmail.com>
2024-10-15 13:20:08 +0100

    added stiorage and naming files stored

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8888 
 const path = require('path')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination(req, file, cb) {
+        cb(null, 'audio');
+    },
+    filename(req, file, cb) {
+        cb(null, `${Date.now()}${path.extname(file.originalname)}.mp3`)
+    }
+
+});
+
+
+const upload = multer({storage});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,6 +26,13 @@ app.get('/ping', function (req, res){
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
+
+app.post('/upload', upload.single('audio'),async function(req, res) {
+    res.send({success: true});
+})
+
+
+
 
 app.listen(PORT, function() {
     console.log(`serv running on port ${PORT}`);
