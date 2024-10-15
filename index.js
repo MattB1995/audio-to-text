93e02ad1d9d9e9c00610ff67378b169ad5b88963
Matt Buczkowski <matbuczkowski95@gmail.com>
2024-10-15 15:47:59 +0100

    added final functionality, apended api key for deepgram

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const app = express()
 const PORT = process.env.PORT || 8888 
 const path = require('path')
 const multer = require('multer')
+const {createClient} = require('@deepgram/sdk')
+const fs = require('fs')
 
+
+//------- multer ------
 const storage = multer.diskStorage({
     destination(req, file, cb) {
-        cb(null, 'audio');
+        cb(null, 'public/audio');
     },
     filename(req, file, cb) {
         cb(null, `${Date.now()}${path.extname(file.originalname)}.mp3`)
@@ -18,7 +22,17 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage});
 
+//----------multer end----------
+
+
+const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+
+
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/ping', function (req, res){
     res.send('pong pong mothafucka');
@@ -28,8 +42,20 @@ app.get('/', function(req, res){
 });
 
 app.post('/upload', upload.single('audio'),async function(req, res) {
-    res.send({success: true});
-})
+
+    const {result, error} = await deepgram.listen.prerecorded.transcribeFile(fs.readFileSync(`./public/audio/${req.file.filename}`),
+
+    {model:'nova-2'}
+);
+
+    if(error) {
+        res.status(500).send('something went wrong')
+    }
+
+    res.send({ 
+        link: `http://localhost:8888/audio/${req.file.filename}`,
+        result: result.results.channels[0].alternatives[0].transcript});
+});
 
 
 
